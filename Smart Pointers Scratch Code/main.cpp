111afd5302707e5645635e153de01d4b26a21b93
aberry5621 <aberry5621@gmail.com>
2017-05-16 13:40:42 -0700

    Init commit with 3 exes

--- Smart Pointers Scratch Code/main.cpp
+++ Smart Pointers Scratch Code/main.cpp
@@ -1,15 +1,69 @@
-//
-//  main.cpp
-//  Smart Pointers Scratch Code
-//
-//  Created by ax on 5/16/17.
-//  Copyright © 2017 COMP235. All rights reserved.
-//
+#define prog 3
 
+#if(prog == 1)
+
+#include <memory>
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    
+    // 3 ways to initialize pointer
+    shared_ptr<string> p1 = nullptr;
+    shared_ptr<string> p2 = 0;
+    shared_ptr<string> p3 = NULL;
+    
+    // allocate and use dynamic memory using make_share
+    p1 = make_shared<string>("");
+    if (p1 && p1->empty()) {
+        *p1 = "HI";
+    }
+    cout << *p1 << endl;
     return 0;
+
 }
+
+#elif(prog == 2)
+
+#include <memory>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    
+    auto ptr1 = make_shared<string>(5, 'Z');
+    
+    cout << *ptr1 << endl;
+    
+    return 0;
+
+}
+
+#elif(prog == 3)
+
+#include <memory>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    
+    auto p = make_shared<int>(42);
+    
+    auto q(p); // copy?
+    
+    cout << "p: " << *p << endl;
+    
+    cout << "q: " << *p << endl;
+    
+    p = nullptr;
+    q = nullptr;
+    
+    return 0;
+    
+}
+
+
+#endif

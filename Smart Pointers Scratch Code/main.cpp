66497d63014455c252cb02c79c48603c5044b515
aberry5621 <aberry5621@gmail.com>
2017-05-16 12:19:02 -0700

    Initial Commit

---
+++ Smart Pointers Scratch Code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Smart Pointers Scratch Code
+//
+//  Created by ax on 5/16/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
